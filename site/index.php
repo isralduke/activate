b4a570ef1b0d33a523c92834b03684abde43ca06
isral C. Duke <isralduke@gmail.com>
2016-01-30 21:38:51 -0600

    initial commit to master of work

---
+++ site/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, userscaleable=no">
+	<title></title>
+	<link rel="author" href="humans.txt" />
+	<meta name="description" content="From websites to wearables, blogs to social networks, mobile apps to ??. Explore the possibilities of interactive design and development. blah blah" />
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/css/normalize.css" />
+	<link rel="stylesheet" href="/css/activate.css" />
+	<?php include('partials/favicons.html'); ?>
+</head>
+<body>
+	<main>
+		<aside>
+			<img src="/assets/img/rocket.svg" />
+		</aside>
+		<article>
+			<section class="info">
+				<h3>baton rouge, la</h3>
+				<h1>activate</h1>
+				<h2>a student conference for all things interactive</h2>
+				<p>From websites to wearables, blogs to social networks, mobile apps to ??. Explore the possibilities of interactive design and development. blah blah.</p>
+			</section>
+			<section class="subscribe">
+				<form action="mail.php" class="form" name="form" id="form" method="post">
+					<div class="formbox group">
+						<label>stay updated</label>
+						<input type="email" placeholder="your email">
+						<button>subscribe</button>
+					</div>
+				</form>
+			</section>
+		</article>
+	</main>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="/js/scripts.js"></script>
+</body>
+</html>
