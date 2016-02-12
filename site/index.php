cb81d9102a4603ee925ed8af47c5954d68bc42df
Lynsey Gwin <lgwin@envoc.com>
2016-02-12 08:44:36 -0600

    style changes, add svg rocket

--- site/index.php
+++ site/index.php
@@ -6,11 +6,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, userscaleable=no">
 	<title></title>
 	<link rel="author" href="humans.txt" />
+	<meta name="robots" content="nofollow" />
 	<meta name="description" content="From websites to wearables, blogs to social networks, mobile apps to ??. Explore the possibilities of interactive design and development. blah blah" />
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,900,700,500,300,100' rel='stylesheet' type='text/css'>
 	<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/3e1612c7-37ae-49f9-b020-c52a55c407de.css"/>
 	<link rel="stylesheet" href="/css/normalize.css" />
 	<link rel="stylesheet" href="/css/activate.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TimelineLite.min.js"></script>
 	<?php include('partials/favicons.html'); ?>
 </head>
 <body>
@@ -21,24 +25,29 @@
 				<h3>baton rouge, la</h3>
 				<h1>activate</h1>
 				<h2>a student conference for all things interactive</h2>
-				<p>From websites to wearables, blogs to social networks, mobile apps to ??. Explore the possibilities of interactive design and development. blah blah.</p>
+				<h4>From websites to mobile apps, wearables to smart devices, social media to virtual reality and beyond. Activate will inspire you to get next-level with your career. Explore the possibilities of UI/UX design, web development, and the exciting future of an interactive world.</h4>
+					<p>We’re looking for self-starters who want to ignite their future beyond the classroom. We’ll have speakers and panelists from the growing interactive community in Baton Rouge talking about their careers, answering your questions, and giving you advice on how to get an edge on the future of design and development. Plus, we’ll give you the resources you need to prepare for a professional career in the interactive design industry. Don’t miss this opportunity to get a jump start on your future.</p>
+
+				<section class="subscribe">
+				<form action="mail.php" class="form" name="form" id="form" method="post">
+					<div class="formbox group">
+						<label>stay updated</label>
+						<input type="email" placeholder="your email">
+						<button>subscribe</button>
+					</div>
+				</form>
+				</section>
 			</section>
 			<section class="rocket">
-				<img src="/assets/img/rocket.svg" />
+				<object type="image/svg+xml" data="/assets/img/rocket.svg" class="animate">
+					<img src="/assets/img/rocket.png" />
+				</object>
+				
 			</section>
+			
 		</section>
-		<section class="subscribe">
-			<form action="mail.php" class="form" name="form" id="form" method="post">
-				<div class="formbox group">
-					<label>stay updated</label>
-					<input type="email" placeholder="your email">
-					<button>subscribe</button>
-				</div>
-			</form>
-		</section>
+
 	</main>
-	<div class="pixels"></div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script src="/js/scripts.js"></script>
 </body>
 </html>
--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,1 +1,10 @@
-scripts.js
+// function activate(){
+  var rocket = '$(svg:hover)',
+    tl = new TimelineLite({});
+
+  tl
+    .to($('#flame'), .1, {scaleY: '1.4', repeat: -1}, 'start')
+    .to($('svg'), .05, { rotation: '+=.7', repeat: -1}, 'start');
+
+  // return tl;
+// }
