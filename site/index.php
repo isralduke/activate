418040dbb9e7fcdf56cc1bed63ddeffd9f7da922
Lynsey Gwin <lgwin@envoc.com>
2016-02-19 17:50:15 -0600

    responsive tweaks

--- site/index.php
+++ site/index.php
@@ -4,10 +4,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, userscaleable=no">
-	<title></title>
+	<title>Activate Conference: A Student Conference for All Things Interactive – Baton Rouge, Louisiana</title>
 	<link rel="author" href="humans.txt" />
-	<meta name="robots" content="nofollow" />
-	<meta name="description" content="From websites to wearables, blogs to social networks, mobile apps to ??. Explore the possibilities of interactive design and development. blah blah" />
+	<meta name="description" content="Activate will inspire you to get next-level with your career. Explore the possibilities of UI/UX design, web development, and the exciting future of an interactive world." />
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,900,700,500,300,100' rel='stylesheet' type='text/css'>
 	<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/3e1612c7-37ae-49f9-b020-c52a55c407de.css"/>
 	<link rel="stylesheet" href="css/normalize.css" />
@@ -26,7 +25,7 @@
 				<h1>activate</h1>
 				<h2>a student conference for all things interactive</h2>
 				<h4>From websites to mobile apps, wearables to smart devices, social media to virtual reality and beyond. Activate will inspire you to get next-level with your career. Explore the possibilities of UI/UX design, web development, and the exciting future of an interactive world.</h4>
-					<p>We’re looking for self-starters who want to ignite their future beyond the classroom. We’ll have speakers and panelists from the growing interactive community in Baton Rouge talking about their careers, answering your questions, and giving you advice on how to get an edge on the future of design and development. Plus, we’ll give you the resources you need to prepare for a professional career in the interactive design industry. Don’t miss this opportunity to get a jump start on your future.</p>
+					<p>We’re looking for self-starters who want to ignite their future beyond the classroom. We’ll have speakers and panelists from the growing interactive community in Baton Rouge talking about their careers, answering your questions, and giving you advice on how to get an edge on the future of design and development. Plus, we’ll give you the resources you need to prepare for a professional career in the interactive industry. Don’t miss this opportunity to get a jump start on your future. <i>Coming Fall 2016.</i></p>
 
 				<section class="subscribe">
 
@@ -181,6 +180,16 @@
 		</section>
 
 	</main>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1373045-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	<script src="js/scripts.js"></script>
 </body>
 </html>
